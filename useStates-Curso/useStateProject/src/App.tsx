907fed76ef36d582fe10bbee0ca2b86f925b7fc4
DiegoCard117 <diegocardoso117@gmail.com>
2023-07-25 18:32:58 -0300

    teste menu

--- useStates-Curso/useStateProject/src/App.tsx
+++ useStates-Curso/useStateProject/src/App.tsx
@@ -7,14 +7,31 @@ function App() {
   const [number, setNumber] = useState(1)
 
   const mais1 = () => {
+    let menu = document.querySelector(".menu")
     //setNumber(number + 1)
     //Previous value - maneira mais correta
     setNumber((prevNumber) => prevNumber + 1)
-    setNumber((prevNumber) => prevNumber + 1)
+    //testes abaixo
+    menu?.classList.toggle("disable")
+    if (document.body.style.backgroundColor != "white") {
+      //document.body.style.backgroundColor = "white"
+      //document.body.style.color = "black"
+      
+    } else {
+      //document.body.style.backgroundColor = "#242424"
+      //document.body.style.color = "white"
+    }
   }
   return (
     <>
-      <h1>Meu nome é {name}</h1>
+      <nav className='menu'>
+        <ul>
+          <li>menu</li>
+          <li>text</li>
+          <li>teste</li>
+        </ul>
+      </nav>
+      <h1>{name}</h1>
       <input 
         type="text" 
         value={name} 
